e547a6b3083b6bce15a78501b90627a1d73f9037
Damián Pumar <damianpumar@gmail.com>
2019-07-18 15:18:02 +0200

    Convert 2 to LCD format Test Red

--- src/test/java/NumberConverterShould.java
+++ src/test/java/NumberConverterShould.java
@@ -1,12 +1,24 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberConverterShould {
+
+    private NumberConverter numberConverter;
+
+    @Before
+    public void setup() {
+        numberConverter = new NumberConverter();
+    }
+
     @Test
     public void be_format_LCD_one() {
-        NumberConverter numberConverter = new NumberConverter();
-
         assertThat(numberConverter.convert(1)).isEqualTo(" \n|\n|");
+    }
+
+    @Test
+    public void be_format_LCD_two() {
+        assertThat(numberConverter.convert(2)).isEqualTo(" _\n _|\n|_");
     }
 }
